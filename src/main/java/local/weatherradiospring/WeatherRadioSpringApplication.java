b3083d38564d3c80441daae666361672d2452b06
shadow3731 <v1rus3731@gmail.com>
2023-03-05 14:28:27 +0300

    Initial commit

---
+++ src/main/java/local/weatherradiospring/WeatherRadioSpringApplication.java
@@ -0,0 +1,13 @@
+package local.weatherradiospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherRadioSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherRadioSpringApplication.class, args);
+    }
+
+}
